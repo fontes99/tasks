b6b21d60c5d9eaa8229fe46d35dcffa4f22c8d21
fontes99 <pvfontes1000@gmail.com>
2020-11-20 14:50:11 +0000

    get done

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ echo '@reboot cd /home/ubuntu/tasks && ./run.sh' | crontab
 export DJANGO_SUPERUSER_PASSWORD=cloud
 export DJANGO_SUPERUSER_USERNAME=cloud
 export DJANGO_SUPERUSER_EMAIL=cloud@a.com
-python3 manage.py createsuperuser --noinput
+#python3 manage.py createsuperuser --noinput
