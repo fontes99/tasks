004cabeb65c9376d0ce93d581e435477f01ffdce
fontes99 <pvfontes1000@gmail.com>
2020-11-20 15:27:42 +0000

    post done

--- tasks/views.py
+++ tasks/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
+from rest_framework.parsers import JSONParser
+
+from django.views.decorators.csrf import csrf_exempt
 from .models import Task
 from .serializers import TasksSerializer
-
-# Create your views here.
-
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the tasks index.")
+from .form import TaskForm
 
 def get_tasks(request):
     items = Task.objects.all()
     serializer = TasksSerializer(items, many =True)
     return JsonResponse(serializer.data, safe =False)
 
+@csrf_exempt
 def post_task(request):
-    pass
+
+    form = TaskForm(request.POST)
+
+    if form.is_valid():
+         form.save()
+         return JsonResponse(form.data,status = 201)
+
+    return JsonResponse(form.errors,status = 400)
